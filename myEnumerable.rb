56b17d39ffabdb70efb193e7064a156f1d8d4cbb
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-03 09:11:43 +0500

    Add methods for any and filter

--- myEnumerable.rb
+++ myEnumerable.rb
@@ -4,4 +4,16 @@ module MyEnumerable
     each { |e| result = false unless yield e }
     result
   end
+
+  def any?
+    result = false
+    each { |e| result = true if yield e }
+    result
+  end
+
+  def filter
+    result = []
+    each { |e| result << e if yield e }
+    result
+  end
 end
